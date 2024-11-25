fe9ce23ef6dc5f5ad4ac3bb9945dc11214216609
Vlad Lysenko <me2ur.cat@yandex.ru>
2024-11-25 18:20:34 +0300

    Работающий вариант с gateway, identity, eureka и recommendation service

--- identity-service/src/main/java/org/example/entity/UserCredentional.java
+++ identity-service/src/main/java/org/example/entity/UserCredentional.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserCredentinal {
+public class UserCredentional {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
